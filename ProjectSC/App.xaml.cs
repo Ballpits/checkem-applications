30f2cee1895597a06ead42123f13d3e89506ce19
Umar Turdiev <aaa392678@gmail.com>
2020-06-26 01:45:02 +0800

    adjust UI

--- ProjectSC/App.xaml.cs
+++ ProjectSC/App.xaml.cs
@@ -12,7 +12,7 @@ namespace ProjectSC
             MainWindow = new MainWindow();
 
             TrayIcon = new System.Windows.Forms.NotifyIcon();
-            TrayIcon.DoubleClick += (s, args) => ShowMainWindow();
+            TrayIcon.DoubleClick += (s, args) => OpenMainWindow();
             TrayIcon.Icon = ProjectSC.Properties.Resources.Icon_16;
             TrayIcon.Visible = true;
 
@@ -23,13 +23,29 @@ namespace ProjectSC
         {
             TrayIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
 
-            TrayIcon.ContextMenuStrip.Items.Add("Check'em").Click += (s, e) => ShowMainWindow();
-            TrayIcon.ContextMenuStrip.Items.Add("Preferences...").Click += (s, e) => ShowMainWindow();
+            TrayIcon.ContextMenuStrip.Items.Add("Check'em").Click += (s, e) => OpenMainWindow();
+            //TrayIcon.ContextMenuStrip.Items.Add("Preferences...").Click += (s, e) => OpenPrefWindow();
             TrayIcon.ContextMenuStrip.Items.Add("-");
             TrayIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ShutdownApplication();
         }
 
-        private void ShowMainWindow()
+        private void OpenMainWindow()
+        {
+            if (MainWindow.IsVisible)
+            {
+                if (MainWindow.WindowState == WindowState.Minimized)
+                {
+                    MainWindow.WindowState = WindowState.Normal;
+                }
+                MainWindow.Activate();
+            }
+            else
+            {
+                MainWindow.Show();
+            }
+        }
+
+        private void OpenPrefWindow()
         {
             if (MainWindow.IsVisible)
             {
