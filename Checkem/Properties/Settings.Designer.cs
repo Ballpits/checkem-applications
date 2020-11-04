1556112d72525552748d5386e86279b6d093536f
Umar Turdiev <aaa392678@gmail.com>
2020-11-04 11:41:14 +0800

    Add settings file to the new version.

--- Checkem/Properties/Settings.Designer.cs
+++ Checkem/Properties/Settings.Designer.cs
@@ -22,5 +22,221 @@ namespace Checkem.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool IsNotificationOn {
+            get {
+                return ((bool)(this["IsNotificationOn"]));
+            }
+            set {
+                this["IsNotificationOn"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Check\'em")]
+        public string ThemeName {
+            get {
+                return ((string)(this["ThemeName"]));
+            }
+            set {
+                this["ThemeName"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool IsDarkModeApplied {
+            get {
+                return ((bool)(this["IsDarkModeApplied"]));
+            }
+            set {
+                this["IsDarkModeApplied"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool IsSavingSortingModeOn {
+            get {
+                return ((bool)(this["IsSavingSortingModeOn"]));
+            }
+            set {
+                this["IsSavingSortingModeOn"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        public int DetailsPanelClosingMode {
+            get {
+                return ((int)(this["DetailsPanelClosingMode"]));
+            }
+            set {
+                this["DetailsPanelClosingMode"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool IsRemovingWaringOn {
+            get {
+                return ((bool)(this["IsRemovingWaringOn"]));
+            }
+            set {
+                this["IsRemovingWaringOn"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("33, 150, 243")]
+        public global::System.Drawing.Color PrimaryColor {
+            get {
+                return ((global::System.Drawing.Color)(this["PrimaryColor"]));
+            }
+            set {
+                this["PrimaryColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("151, 8, 204")]
+        public global::System.Drawing.Color SecondaryColor {
+            get {
+                return ((global::System.Drawing.Color)(this["SecondaryColor"]));
+            }
+            set {
+                this["SecondaryColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("White")]
+        public global::System.Drawing.Color LightMainColor {
+            get {
+                return ((global::System.Drawing.Color)(this["LightMainColor"]));
+            }
+            set {
+                this["LightMainColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("237, 247, 255")]
+        public global::System.Drawing.Color TransparentHighlightColor {
+            get {
+                return ((global::System.Drawing.Color)(this["TransparentHighlightColor"]));
+            }
+            set {
+                this["TransparentHighlightColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Black")]
+        public global::System.Drawing.Color DarkMainColor {
+            get {
+                return ((global::System.Drawing.Color)(this["DarkMainColor"]));
+            }
+            set {
+                this["DarkMainColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("150, 150, 150")]
+        public global::System.Drawing.Color ItemCompletedTextColor {
+            get {
+                return ((global::System.Drawing.Color)(this["ItemCompletedTextColor"]));
+            }
+            set {
+                this["ItemCompletedTextColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Red")]
+        public global::System.Drawing.Color ItemPassedTextColor {
+            get {
+                return ((global::System.Drawing.Color)(this["ItemPassedTextColor"]));
+            }
+            set {
+                this["ItemPassedTextColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Transparent")]
+        public global::System.Drawing.Color NavigationbarButtonColor {
+            get {
+                return ((global::System.Drawing.Color)(this["NavigationbarButtonColor"]));
+            }
+            set {
+                this["NavigationbarButtonColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("76, 255, 255, 255")]
+        public global::System.Drawing.Color NavigationbarButtonHighlightColor {
+            get {
+                return ((global::System.Drawing.Color)(this["NavigationbarButtonHighlightColor"]));
+            }
+            set {
+                this["NavigationbarButtonHighlightColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("WhiteSmoke")]
+        public global::System.Drawing.Color GrayMainColor {
+            get {
+                return ((global::System.Drawing.Color)(this["GrayMainColor"]));
+            }
+            set {
+                this["GrayMainColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("White")]
+        public global::System.Drawing.Color ItembarColor {
+            get {
+                return ((global::System.Drawing.Color)(this["ItembarColor"]));
+            }
+            set {
+                this["ItembarColor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Lng_English_US")]
+        public string CurrentLanguage {
+            get {
+                return ((string)(this["CurrentLanguage"]));
+            }
+            set {
+                this["CurrentLanguage"] = value;
+            }
+        }
     }
 }
