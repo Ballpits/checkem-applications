029a0887331a7385a178576df310bad75396cd52
Umar Turdiev <aaa392678@gmail.com>
2020-11-04 12:08:40 +0800

    Copy Mainwindow UI from the old one.

--- Checkem/MainWindow.xaml.cs
+++ Checkem/MainWindow.xaml.cs
@@ -1,35 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Checkem
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            BlackTheme();
         }
 
+        private void BlackTheme()
+        {
+            Properties.Settings.Default.PrimaryColor = System.Drawing.Color.Black;
+            Properties.Settings.Default.SecondaryColor = System.Drawing.Color.Black;
+            Properties.Settings.Default.LightMainColor = System.Drawing.Color.Black;
+            Properties.Settings.Default.DarkMainColor = System.Drawing.Color.White;
+            Properties.Settings.Default.GrayMainColor = System.Drawing.Color.Black;
+            Properties.Settings.Default.ItembarColor = System.Drawing.Color.Black;
+
+            Properties.Settings.Default.Save();
+        }
+
+
         private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            //timer goes here
+        }
+
+
+
+        #region Window chrome buttons
+
+        private void ButtonSettings_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void RestoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+
+
+        #region Resize and state changed events
         private void AppWindow_StateChanged(object sender, EventArgs e)
         {
             if (this.WindowState == WindowState.Maximized)
@@ -60,6 +98,13 @@ namespace Checkem
                 SearchBoxBorder.HorizontalAlignment = HorizontalAlignment.Left;
             }
         }
+        #endregion
+
+
+
+        #region Navigation bar
+
+        #region Expander buttons
 
         private void ButtonExpand_Click(object sender, RoutedEventArgs e)
         {
@@ -67,50 +112,64 @@ namespace Checkem
             ButtonExpand.Visibility = Visibility.Collapsed;
         }
 
+
         private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
         {
             ButtonCollaps.Visibility = Visibility.Collapsed;
             ButtonExpand.Visibility = Visibility.Visible;
         }
 
+        #endregion
+
+        #region Buttons
+
         private void ButtonMyDay_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+
         private void ButtonAllTasks_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+
         private void ButtonDueDateFilter_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
 
         private void ButtonStarredFilter_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void ButtonSettings_Click(object sender, RoutedEventArgs e)
+
+        private void ButtonAddNewList_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        public void ChangeFilter(int mode)
-        {
-
-        }
+        #endregion
 
         private void MoveNavbarCursor(int index)
         {
 
         }
 
-        private void ButtonAddNewList_Click(object sender, RoutedEventArgs e)
+
+        public void ChangeFilter(int mode)
         {
 
         }
+
+        #endregion
+
+
+
+        #region Searchbox
 
         private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -121,6 +180,12 @@ namespace Checkem
         {
 
         }
+
+        #endregion
+
+
+
+        #region Hotkeys
 
         bool IsLCtrlPressed = false;
         bool IsFPressed = false;
@@ -173,26 +238,6 @@ namespace Checkem
             }
         }
 
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
-
-        private void RestoreButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.WindowState == WindowState.Maximized)
-            {
-                this.WindowState = WindowState.Normal;
-            }
-            else
-            {
-                this.WindowState = WindowState.Maximized;
-            }
-        }
-
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
+        #endregion
     }
 }
