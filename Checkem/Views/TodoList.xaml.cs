24eecda2fdd45973ca670fa19c4a867bf9dbc4ac
Umar Turdiev <ukcturdiev@icloud.com>
2020-12-04 15:51:44 +0800

    Item counter for to do list.

--- Checkem/Views/TodoList.xaml.cs
+++ Checkem/Views/TodoList.xaml.cs
@@ -2,26 +2,89 @@
 using Checkem.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace Checkem.Views
 {
-    public partial class TodoList : UserControl
+    public partial class TodoList : UserControl, INotifyPropertyChanged
     {
         public TodoList()
         {
+            DataContext = this;
+
             InitializeComponent();
         }
 
 
+        #region Event
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+
+        #region Variable
         List<ToDoItem> currentInventory = TodoManager.Filter(FilterMethods.Starred);
+        #endregion
+
+
+        #region Property
+        public string ItemCount
+        {
+            get
+            {
+                if (currentInventory.Count > 0)
+                {
+                    if (ToDoListItemCounterTextBlock.Margin == new Thickness(0))
+                    {
+                        ToDoListItemCounterTextBlock.Margin = new Thickness(0, 0, 5, 0);
+                    }
+                    return currentInventory.Count.ToString();
+                }
+                else
+                {
+                    ToDoListItemCounterTextBlock.Margin = new Thickness(0);
+                    return string.Empty;
+                }
+            }
+        }
+
+        public string ItemCounterString
+        {
+            get
+            {
+                if (currentInventory.Count == 1)
+                {
+                    return "Task";
+                }
+                else if (currentInventory.Count > 1)
+                {
+                    return "Tasks";
+                }
+                else
+                {
+                    return "Nothing in the list";
+                }
+            }
+        }
+        #endregion
+
 
 
         private void TodoList_Loaded(object sender, RoutedEventArgs e)
         {
             LoadTodoList();
+
+            OnPropertyChanged();
         }
+
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
 
         private void LoadTodoList()
         {
