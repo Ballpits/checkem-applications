20464ddc51ff94721b411579d8700739ae225ea0
Umar Turdiev <ukcturdiev@icloud.com>
2021-01-01 17:05:24 +0800

    Implement calendar components.

--- Checkem/CustomComponents/CalendarComponents/CalendarComponents_Day.xaml.cs
+++ Checkem/CustomComponents/CalendarComponents/CalendarComponents_Day.xaml.cs
@@ -1,28 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Checkem.CustomComponents.CalendarComponents
 {
-    /// <summary>
-    /// Interaction logic for CalendarComponents_Day.xaml
-    /// </summary>
     public partial class CalendarComponents_Day : UserControl
     {
         public CalendarComponents_Day()
         {
+            DataContext = this;
+
             InitializeComponent();
         }
+
+
+
+        public string Day
+        {
+            get { return (string)GetValue(DayProperty); }
+            set { SetValue(DayProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Day.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DayProperty =
+            DependencyProperty.Register("Day", typeof(string), typeof(CalendarComponents_Day), new PropertyMetadata(string.Empty));
     }
 }
