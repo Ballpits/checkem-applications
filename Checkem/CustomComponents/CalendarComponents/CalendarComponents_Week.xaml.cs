6f7bbeefdd6892bed67679ba263a55154d7f8cff
Umar Turdiev <ukcturdiev@icloud.com>
2021-01-05 08:52:14 +0800

    Dark theme xaml file.

--- Checkem/CustomComponents/CalendarComponents/CalendarComponents_Week.xaml.cs
+++ Checkem/CustomComponents/CalendarComponents/CalendarComponents_Week.xaml.cs
@@ -15,14 +15,33 @@ using System.Windows.Shapes;
 
 namespace Checkem.CustomComponents.CalendarComponents
 {
-    /// <summary>
-    /// Interaction logic for CalendarComponents_Week.xaml
-    /// </summary>
     public partial class CalendarComponents_Week : UserControl
     {
         public CalendarComponents_Week()
         {
             InitializeComponent();
+        }
+
+
+
+        public int Week
+        {
+            get { return (int)GetValue(WeekProperty); }
+            set
+            {
+                SetValue(WeekProperty, value);
+                SetupDays();
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for Week.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WeekProperty =
+            DependencyProperty.Register("Week", typeof(int), typeof(CalendarComponents_Week), new PropertyMetadata(1));
+
+
+        private void SetupDays()
+        {
+
         }
     }
 }
