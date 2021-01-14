de5c37e05ec4f3dae74c635e76ff6bdab85c8a81
Umar Turdiev <ukcturdiev@icloud.com>
2021-01-14 23:17:56 +0800

    Added test class for sphere.data.

---
+++ Library.Tests/SphereData_Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sphere.Data;
+using Xunit;
+
+namespace Library.Tests
+{
+    public class SphereData_Tests
+    {
+        public void Test_WillAddItemIntoJson()
+        {
+
+        }
+    }
+}
